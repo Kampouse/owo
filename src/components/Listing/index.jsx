efa1bff0537f9efe847957e24c59800943a0a983
Alexandre Consigny <alexandre.consigny@gmail.com>
2024-05-05 21:41:50 -0400

    fix: use correct date for ReactTimeAgo

--- src/components/Listing/index.jsx
+++ src/components/Listing/index.jsx
@@ -12,35 +12,37 @@ const borderTypes = {
 
 const getTypeColor = (type) => type && type.length ? borderTypes[type[0]] : 'primary'
 
-const Listing = ({ listing: { id, title, description, type, tags, userProfile, created_at, images }, children, noProfile }) => (
-  <ListingLayout
-    title={title}
-    image={images && images.length > 0 && images[0]}
-    description={description}
-    footer={
-      <Row className="align-items-center">
-        {!noProfile &&
-          <>
-            <div style={{ width: '50px', display: 'flex', alignItems: 'center' }} className="mr-2">
-              <Card.Img
-                src={"https://api.multiavatar.com/" + userProfile.id + '.png'}
-                alt="avatar"
-                className="rounded-circle"
-                style={{ width: '50px', height: 'auto' }}
-              />
-            </div>
-            <Col>
-              <span>{userProfile.username}</span> <span className="text-muted">📍 Habitus🌿</span> <br />
-              <span className="text-muted"><ReactTimeAgo date={new Date(created_at).getDate()} locale="fr" /></span>
-            </Col>
-          </>
-        }
-        <Col className="text-right">
-          {children}
-        </Col>
-      </Row>
-    }
-  />
-);
+const Listing = ({ listing: { id, title, description, type, tags, userProfile, created_at, images }, children, noProfile }) => {
+  return (
+    <ListingLayout
+      title={title}
+      image={images && images.length > 0 && images[0]}
+      description={description}
+      footer={
+        <Row className="align-items-center">
+          {!noProfile &&
+            <>
+              <div style={{ width: '50px', display: 'flex', alignItems: 'center' }} className="mr-2">
+                <Card.Img
+                  src={"https://api.multiavatar.com/" + userProfile.id + '.png'}
+                  alt="avatar"
+                  className="rounded-circle"
+                  style={{ width: '50px', height: 'auto' }}
+                />
+              </div>
+              <Col>
+                <span>{userProfile.username}</span> <span className="text-muted">📍 Habitus🌿</span> <br />
+                <span className="text-muted"><ReactTimeAgo date={new Date(created_at)} locale="fr" /></span>
+              </Col>
+            </>
+          }
+          <Col className="text-right">
+            {children}
+          </Col>
+        </Row>
+      }
+    />
+  )
+}
 
 export default Listing;
