d93fb05a4e334799f11ea0344bd2f262b42b63c8
Alexandre Consigny <alexandre.consigny@gmail.com>
2024-05-15 21:01:39 -0400

    fix: rehydrate

--- src/components/Notifications/SingleNotification.jsx
+++ src/components/Notifications/SingleNotification.jsx
@@ -1,21 +1,24 @@
 import Toast from 'react-bootstrap/Toast';
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactTimeAgo from 'react-time-ago'
 import { IoIosChatbubbles } from 'react-icons/io';
 import Link from 'next/link'
 import { viewNotification } from '@/notifications/UserNotificationClient';
 
-
 const SingleNotification = ({ id, excerpt, createdAt, status, type, context, floating }) => {
   const [show, setShow] = useState(true)
+  const [isClient, setIsClient] = useState(false)
   const floatingProps = floating ? {
     onClose: () => setShow(false),
     delay: 7000,
     autohide: true,
   } : {}
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
-
-  return (
+  return isClient ? (
     <Link href={type === 'message' && `/messages/${context.conversationId}`} onClick={() => viewNotification(id)}>
       <Toast {...floatingProps} show={show}>
         <Toast.Header closeButton={false}>
@@ -30,7 +33,7 @@ const SingleNotification = ({ id, excerpt, createdAt, status, type, context, flo
         <Toast.Body>{type === 'message' && "Nouveau message"}: {excerpt}</Toast.Body>
       </Toast>
     </Link>
-  );
+  ) : null;
 }
 
 
