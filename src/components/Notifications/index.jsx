2a24ceafc69620aca67003b1d812500eb65cdf25
melonique <melonique@gmail.com>
2024-05-15 23:03:00 -0400

    clean bootstrap leftovers

--- src/components/Notifications/index.jsx
+++ src/components/Notifications/index.jsx
@@ -1,15 +1,14 @@
 'use client'
-import React from 'react';
+
 import SingleNotification from './SingleNotification';
-import { Row, Col, Container } from "react-bootstrap";
 
 const Notifications = ({ notifications }) => {
   return (
-    <Container>
+    <>
       {notifications.map((notification, index) => (
-        <Row key={index} className="pt-3"><Col><SingleNotification  {...notification} /></Col></Row>
+        <SingleNotification  {...notification} key={`notiflist-${i}`} />
       ))}
-    </Container>
+    </>
   )
 }
 
