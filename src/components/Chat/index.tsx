adc31b17632cb11daeaf1f0376c03b74e2d0084b
melonique <melonique@gmail.com>
2024-05-15 23:55:21 -0400

    refactor chatuserlist for shadcn

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useEffect } from "react";
-import { Row, Col, Card, Offcanvas } from "react-bootstrap";
+import React, { useState } from "react";
 import { useChat } from "@/contexts/ChatContext";
 
 import ChatWithUser from './ChatWithUser'
@@ -20,28 +19,17 @@ const Chat: React.FC = () => {
   }
 
   return (
-    <Row>
+    <>
 
-      <Col md="4" lg="5" xl="4">
-        <Offcanvas show={show} onHide={handleClose} responsive="md">
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title></Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            <Card style={{ width: '100%' }}>
-              <ChatUserList conversations={conversations} currentChatId={currentChatId} onSelect={handleClose} conversationsByStatus={conversationsByStatus} />
-            </Card>
-          </Offcanvas.Body>
-        </Offcanvas>
-      </Col>
+      <ChatUserList isCollapsed={!show} conversations={conversations} currentChatId={currentChatId} onSelect={handleClose} conversationsByStatus={conversationsByStatus} />
 
-      <Col sm="12" md="8" lg="7" xl="8" className="col-chat">
+
           {isSelectedConversationBot
             ? <ChatWithBot showNav={handleShow} />
             : <ChatWithUser conversation={conversation} currentUser={currentUser} showNav={handleShow} />
           }
-        </Col>
-    </Row>
+
+    </>
 
   );
 };
